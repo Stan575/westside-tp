87e3ebc1a2441cb5a528e982e3bd43fc476994a5
rusll-repo <rusllcode@gmail.com>
2022-04-18 22:52:42 -0700

    add test balance bracets

--- unit/test/test_median.py
+++ unit/test/test_median.py
@@ -1,5 +1,6 @@
 import unittest
 from unit.src.medain import median
+from unit.src.balanced_brackets import is_balanced
 
 
 class TestMedian(unittest.TestCase):
@@ -10,6 +11,13 @@ class TestMedian(unittest.TestCase):
     def test_median_list_single_element(self):
         self.assertEqual(median([2]), 2)
 
+class Test_Balanced_Bracets(unittest.TestCase):
 
+    def test_balance(self):
+        self.assertEqual(is_balanced('{{[([())]]}}'), False)
+
+    def test_balance2(self):
+        self.assertEqual(is_balanced('{{[[(())]]}}'), True)
+        
 if __name__ == '__main__':
     unittest.main()
