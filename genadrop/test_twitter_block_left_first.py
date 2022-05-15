7437ee246f52aa5012808ca2f962ff1b5f3c5a9e
valeriy Kan <valeriy.kan@gmail.com>
2022-05-14 20:00:30 -0400

    test twitter block

--- genadrop/test_twitter_block_left_first.py
+++ genadrop/test_twitter_block_left_first.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -18,39 +19,43 @@ class TestLogo(unittest.TestCase):
         self.wait = WebDriverWait(self.driver, 5)
         self.driver.set_window_size(1600, 1050)
         self.driver.get(self.URL)
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[class^='Navbar_container']")))
 
     def tearDown(self):
         self.driver.quit()
 
     def test_tweet_redirect_first_left(self):
+        """ """
+        # save window handle
         original_window = self.driver.current_window_handle
-
+        # scroll down to widgets
+        SU.scroll(SU(self.driver), (By.XPATH, "(//iframe)[1]"))
+        # save iframe #1 and switch to
         iframe = self.wait.until(EC.presence_of_element_located((By.ID, "twitter-widget-0")))
-
         self.driver.switch_to.frame(iframe)
-
-        # self.wait.until(EC.element_to_be_clickable((By.XPATH, "//article/div/div[@lang='en']"))).send_keys('\n')
-        self.wait.until(EC.element_to_be_clickable((By.TAG_NAME, "article"))).send_keys('\n')
-
-
-        widget_twit = self.driver.find_element(By.XPATH, "//article/div/div[@lang='en']")
-
-        # print('====>', widget_twit.text)
-        widget_twit_text = widget_twit.text[0:30]
+        # click on widget
+        self.driver.find_element(By.TAG_NAME, "article").click()
+        #save text from twit - first 30 substring
+        widget_twit_text = (self.driver.find_element(By.XPATH, "//article/div/div[@lang='en']")).text[0:30]
+        # switch to opened new tab
         self.driver.switch_to.window(self.driver.window_handles[1])
-
+        # get text of twit title
         tweet_h2_text = self.wait.until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "h2>span")))
-        # tweet_on_twitter_page = self.wait.until(
-        #     EC.presence_of_element_located((By.XPATH, '(//div[@data-testid="tweetText"])[1]')))
+        # get element - twit's article
         tweet_on_twitter_page = self.wait.until(
             EC.presence_of_element_located((By.XPATH, '(//article/div)[1]//div[@lang="en"]')))
-        # print('2====>',tweet_on_twitter_page.text)
-        self.assertEqual('Tweet', tweet_h2_text.text, 'Text are not equals!')
-        self.assertTrue(tweet_on_twitter_page.text.startswith(widget_twit_text))
 
+        # Assert - verify twit's title
+        self.assertEqual('Tweet', tweet_h2_text.text, 'Text are not equals!')
+        # Assert - verify article text starts with text from widget
+        self.assertTrue(tweet_on_twitter_page.text.startswith(widget_twit_text),"Opened wrong Twitter article!")
+
+        # close opened tab
         self.driver.close()
+        # get back to first window(iframe)
         self.driver.switch_to.window(original_window)
+        # return to the default content(quit from iframe)
         self.driver.switch_to.default_content()
 
 
